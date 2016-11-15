c71c0e42e07f9023c4b865a79729fa90c8c057a6
rafa <rafa@rafa-K55A>
2016-11-15 12:14:03 +0100

    new with parameter

--- public_html/js/documento/new.js
+++ public_html/js/documento/new.js
@@ -1,21 +1,21 @@
-/* 
+/*
  * Copyright (c) 2015 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
- * 
- * eritay: The stunning micro-library that helps you to develop easily 
+ *
+ * eritay: The stunning micro-library that helps you to develop easily
  *             AJAX web applications by using Java and jQuery
  * eritay is distributed under the MIT License (MIT)
  * Sources at https://github.com/rafaelaznar/
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,7 +23,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * 
+ *
  */
 
 'use strict';
@@ -36,12 +36,27 @@ moduloDocumento.controller('DocumentoNewController', ['$scope', '$routeParams', 
 
         $scope.title = "Creación de un nuevo documento";
         $scope.icon = "fa-file-text-o";
-      
+
         $scope.result = null;
 
         $scope.obj = {};
         $scope.obj.obj_tipodocumento = {"id": 0};
         $scope.obj.obj_usuario = {"id": 0};
+
+
+        if ($routeParams.id_usuario) {
+            serverService.promise_getOne('usuario', $routeParams.id_usuario).then(function (response) {
+                if (response.data.message.id != 0) {
+                    $scope.obj.obj_usuario = response.data.message;
+                    $scope.show_obj_usuario = false;
+                    $scope.title = "Nuevo documento del usuario " + $scope.obj.obj_usuario.nombre + ' ' + $scope.obj.obj_usuario.apellidos;
+                }
+            });
+        } else {
+            $scope.show_obj_usuario = true;
+        }
+
+
 
         if (sharedSpaceService.getFase() == 0) {
             if ($routeParams.tipodocumento && $routeParams.tipodocumento > 0) {
@@ -74,32 +89,32 @@ moduloDocumento.controller('DocumentoNewController', ['$scope', '$routeParams', 
         };
 
         $scope.$watch('obj.obj_tipodocumento.id', function () {
-            if ($scope.obj) {       
-                    serverService.promise_getOne('tipodocumento', $scope.obj.obj_tipodocumento.id).then(function (response) {
-                        var old_id = $scope.obj.obj_tipodocumento.id;
-                        $scope.obj.obj_tipodocumento = response.data.message;
-                        if (response.data.message.id != 0) {
-                            $scope.outerForm.obj_tipodocumento.$setValidity('exists', true);
-                        } else {
-                            $scope.outerForm.obj_tipodocumento.$setValidity('exists', false);
-                            $scope.obj.obj_tipodocumento.id = old_id;
-                        }
-                    });            
+            if ($scope.obj) {
+                serverService.promise_getOne('tipodocumento', $scope.obj.obj_tipodocumento.id).then(function (response) {
+                    var old_id = $scope.obj.obj_tipodocumento.id;
+                    $scope.obj.obj_tipodocumento = response.data.message;
+                    if (response.data.message.id != 0) {
+                        $scope.outerForm.obj_tipodocumento.$setValidity('exists', true);
+                    } else {
+                        $scope.outerForm.obj_tipodocumento.$setValidity('exists', false);
+                        $scope.obj.obj_tipodocumento.id = old_id;
+                    }
+                });
             }
         });
 
-        $scope.$watch('obj.obj_usuario.id', function () {            
-            if ($scope.obj) {        
-                    serverService.promise_getOne('usuario', $scope.obj.obj_usuario.id).then(function (response) {
-                        var old_id = $scope.obj.obj_usuario.id;
-                        $scope.obj.obj_usuario = response.data.message;
-                        if (response.data.message.id != 0) {
-                            $scope.outerForm.obj_usuario.$setValidity('exists', true);
-                        } else {
-                            $scope.outerForm.obj_usuario.$setValidity('exists', false);
-                            $scope.obj.obj_usuario.id = old_id;
-                        }
-                    });  
+        $scope.$watch('obj.obj_usuario.id', function () {
+            if ($scope.obj) {
+                serverService.promise_getOne('usuario', $scope.obj.obj_usuario.id).then(function (response) {
+                    var old_id = $scope.obj.obj_usuario.id;
+                    $scope.obj.obj_usuario = response.data.message;
+                    if (response.data.message.id != 0) {
+                        $scope.outerForm.obj_usuario.$setValidity('exists', true);
+                    } else {
+                        $scope.outerForm.obj_usuario.$setValidity('exists', false);
+                        $scope.obj.obj_usuario.id = old_id;
+                    }
+                });
             }
         });
 
@@ -114,7 +129,7 @@ moduloDocumento.controller('DocumentoNewController', ['$scope', '$routeParams', 
         };
 
 
-        //datepicker 1 
+        //datepicker 1
         $scope.open1 = function () {
             $scope.popup1.opened = true;
         };
@@ -126,7 +141,7 @@ moduloDocumento.controller('DocumentoNewController', ['$scope', '$routeParams', 
             startingDay: 1
         };
 
-        //datepicker 2 
+        //datepicker 2
         $scope.open2 = function () {
             $scope.popup2.opened = true;
         };
